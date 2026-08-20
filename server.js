40afda419240cba8e3b7ebf6d63ab08fffa9d303
Hithem007 <hithemal@buffalo.edu>
2026-08-20 11:25:38 -0400

    Configure CORS with specific options

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: false
+}));
 app.use(express.json());
 
 // Health check
